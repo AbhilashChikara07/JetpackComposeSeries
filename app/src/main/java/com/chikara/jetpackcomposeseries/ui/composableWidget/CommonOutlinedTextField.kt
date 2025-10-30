718b3828f9a762ee0c81534eaddebae30c73b140
Gulshan kumar <gulshan.kumar@uolo.com>
2025-10-30 15:41:54 +0530

    Make changes for textFormField

--- app/src/main/java/com/chikara/jetpackcomposeseries/ui/composableWidget/CommonOutlinedTextField.kt
+++ app/src/main/java/com/chikara/jetpackcomposeseries/ui/composableWidget/CommonOutlinedTextField.kt
@@ -1,18 +1,16 @@
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.unit.dp
 import com.chikara.jetpackcomposeseries.R
 import com.chikara.jetpackcomposeseries.ui.theme.AppTypography
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
 
 @Composable
 fun CommonOutlinedTextField(
@@ -22,6 +20,9 @@ fun CommonOutlinedTextField(
     keyboardType: KeyboardType = KeyboardType.Text
 ) {
     val statusBarColor = colorResource(R.color.purple_100)
+
+    // 👁 For toggling password visibility
+    var passwordVisible by remember { mutableStateOf(false) }
 
     OutlinedTextField(
         value = value,
@@ -33,6 +34,15 @@ fun CommonOutlinedTextField(
                 style = AppTypography.headlineMedium[14]!!
             )
         },
+        singleLine = true,
+        shape = RoundedCornerShape(8.dp),
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+
+        // 🔒 Hide password if keyboardType is Password
+        visualTransformation = if (keyboardType == KeyboardType.Password && !passwordVisible)
+            PasswordVisualTransformation()
+        else
+            VisualTransformation.None,
 
         colors = TextFieldDefaults.colors(
             focusedIndicatorColor = colorResource(R.color.purple_400),
@@ -40,13 +50,29 @@ fun CommonOutlinedTextField(
             focusedContainerColor = statusBarColor,
             unfocusedContainerColor = statusBarColor,
             focusedLabelColor = colorResource(R.color.purple_400),
+            unfocusedLabelColor = colorResource(R.color.purple_400),
             focusedTextColor = colorResource(R.color.purple_400),
             unfocusedTextColor = colorResource(R.color.purple_400),
             cursorColor = colorResource(R.color.purple_400)
         ),
-        singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
         modifier = Modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(8.dp)
+
+        // 👁 Add trailing icon for show/hide password
+//        trailingIcon = {
+//            if (keyboardType == KeyboardType.Password) {
+//                val image = if (passwordVisible)
+//                    painterResource(id = R.drawable.ic_visibility)
+//                else
+//                    painterResource(id = R.drawable.ic_visibility_off)
+//
+//                IconButton(onClick = { passwordVisible = !passwordVisible }) {
+//                    Icon(
+//                        painter = image,
+//                        contentDescription = if (passwordVisible) "Hide password" else "Show password",
+//                        tint = colorResource(R.color.purple_400)
+//                    )
+//                }
+//            }
+//        }
     )
 }
